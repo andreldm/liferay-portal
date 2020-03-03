13f2da1af4ac2645075531d9f94c74f567a02f65
Andre Miranda <andre.miranda@liferay.com>
2020-03-03 16:20:59 -0300

    LRAC-5214 Save channelId

---
+++ modules/apps/analytics/analytics-settings-api/src/main/java/com/liferay/analytics/settings/constants/AnalyticsSettingsConstants.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.settings.constants;
+
+/**
+ * @author André Miranda
+ */
+public class AnalyticsSettingsConstants {
+
+	public static final String ANALYTICS_TYPE_SETTINGS_CHANNEL_ID =
+		"ANALYTICS_TYPE_SETTINGS_CHANNEL_ID";
+
+}
